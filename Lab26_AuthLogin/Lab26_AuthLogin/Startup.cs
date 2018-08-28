dae1fec4a5128f7ad09eef8d5335f48dad300a79
Mozzey <mbcacciano@gmail.com>
2018-08-28 13:32:19 -0400

    login page with authentication

---
+++ Lab26_AuthLogin/Lab26_AuthLogin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab26_AuthLogin.Startup))]
+namespace Lab26_AuthLogin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
